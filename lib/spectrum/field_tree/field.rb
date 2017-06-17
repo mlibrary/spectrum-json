02775d2882e8b612c8f7f4a77a9843d93b98a671
Albert Bertram <bertrama@umich.edu>
2017-06-17 14:46:11 -0400

    Update to handle by_uid returning nil when not found.

--- lib/spectrum/field_tree/field.rb
+++ lib/spectrum/field_tree/field.rb
@@ -11,17 +11,17 @@ module Spectrum
 
       def params(field_map)
         ret = super
-        unless @value.empty? || field_map.by_uid(@value).empty?
+        unless @value.empty? || field_map.by_uid(@value).nil?
           ret.merge!(field_map.by_uid(@value).query_params)
         end
         ret
       end
 
       def query(field_map)
-        if @value.empty? || field_map.by_uid(@value).empty?
+        if @value.empty? || field_map.by_uid(@value).nil?
           @children.map {|item| item.query(field_map)}.join(' ')
         else
-          "#{field_map.by_uid(@value).field || @value}:(#{@children.map {|item| item.query(field_map)}.join(' ')})"
+          "#{field_map.by_uid(@value).field}:(#{@children.map {|item| item.query(field_map)}.join(' ')})"
         end
       end
     end
