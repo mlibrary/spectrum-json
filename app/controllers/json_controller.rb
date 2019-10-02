9f93e224483ca7aa3e5aed600a2c4a0d6a33bb75
Albert Bertram <bertrama@umich.edu>
2019-10-01 22:33:48 -0400

    Putting in the affiliation default.

--- app/controllers/json_controller.rb
+++ app/controllers/json_controller.rb
@@ -3,6 +3,31 @@
 class JsonController < ApplicationController
   before_filter :init, :sample, :cors
 
+  AA_ADDRESSES = [
+    IPAddr.new('35.0.0.0/16'),
+    IPAddr.new('35.1.0.0/16'),
+    IPAddr.new('35.2.0.0/16'),
+    IPAddr.new('35.3.0.0/16'),
+    IPAddr.new('67.194.0.0/16'),
+    IPAddr.new('141.211.0.0/16'),
+    IPAddr.new('141.212.0.0/16'),
+    IPAddr.new('141.213.0.0/16'),
+    IPAddr.new('141.214.0.0/16'),
+    IPAddr.new('192.12.80.0/24'),
+    IPAddr.new('198.108.8.0/21'),
+    IPAddr.new('198.111.224.0/22'),
+    IPAddr.new('198.111.181.0/25'),
+    IPAddr.new('207.75.144.0/20'),
+    IPAddr.new('10.0.0.0/8'),
+    IPAddr.new('127.0.0.0/8'),
+    IPAddr.new('172.16.0.0/12'),
+    IPAddr.new('192.168.0.0/16')
+  ]
+
+  FLINT_ADDRESSES = [
+    IPAddr.new('141.216.0.0/16')
+  ]
+
   def cors
     headers['Access-Control-Allow-Origin'] = get_origin(request.headers)
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Referer'
@@ -231,37 +256,33 @@ class JsonController < ApplicationController
     { base_url: config.relative_url_root }
   end
 
+  def get_ip_addr
+    if request.env['REMOTE_ADDR'] != '127.0.0.1'
+      request.env['REMOTE_ADDR']
+    elsif request.env['HTTP_X_FORWARDED_FOR']
+      request.env['HTTP_X_FORWARDED_FOR'].split(/ /).last
+    else
+      '127.0.0.1'
+    end
+  end
+
+  def default_affiliation
+    case IPAddr.new(get_ip_addr)
+    when *AA_ADDRESSES
+      'aa'
+    when *FLINT_ADDRESSES
+      'flint'
+    else
+      'aa'
+    end
+  end
+
   # TODO: Move this into configuration.
   def default_institution
-    addr = if request.env['REMOTE_ADDR'] != '127.0.0.1'
-             request.env['REMOTE_ADDR']
-           elsif request.env['HTTP_X_FORWARDED_FOR']
-             request.env['HTTP_X_FORWARDED_FOR'].split(/ /).last
-           else
-             '127.0.0.1'
-    end
-
-    case IPAddr.new(addr)
-    when IPAddr.new('35.0.0.0/16'),
-         IPAddr.new('35.1.0.0/16'),
-         IPAddr.new('35.2.0.0/16'),
-         IPAddr.new('35.3.0.0/16'),
-         IPAddr.new('67.194.0.0/16'),
-         IPAddr.new('141.211.0.0/16'),
-         IPAddr.new('141.212.0.0/16'),
-         IPAddr.new('141.213.0.0/16'),
-         IPAddr.new('141.214.0.0/16'),
-         IPAddr.new('192.12.80.0/24'),
-         IPAddr.new('198.108.8.0/21'),
-         IPAddr.new('198.111.224.0/22'),
-         IPAddr.new('198.111.181.0/25'),
-         IPAddr.new('207.75.144.0/20'),
-         IPAddr.new('10.0.0.0/8'),
-         IPAddr.new('127.0.0.0/8'),
-         IPAddr.new('172.16.0.0/12'),
-         IPAddr.new('192.168.0.0/16')
+    case IPAddr.new(get_ip_addr)
+    when *AA_ADDRESSES
       'U-M Ann Arbor Libraries'
-    when IPAddr.new('141.216.0.0/16')
+    when *FLINT_ADDRESSES
       'Flint Thompson Library'
     else
       'All libraries'
@@ -274,7 +295,8 @@ class JsonController < ApplicationController
       response: @response.spectrum(filter_limit: -1),
       messages: @messages.map(&:spectrum),
       total_available: @response.total_available,
-      default_institution: default_institution
+      default_institution: default_institution,
+      affiliation: default_affiliation,
     }
   end
 
