fd871cd61fedf63f7e12579864f6644889e44303
Monique Rio <mrio@umich.edu>
2020-08-25 17:07:50 -0400

    added github dependencies to Gemfile; made get_this tests work with full rspec tests

--- lib/spectrum/response/get_this.rb
+++ lib/spectrum/response/get_this.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-#FIXME
-#require 'aleph'
+require 'aleph'
 
 module Spectrum
   module Response
--- spec/spectrum/response/get_this_spec.rb
+++ spec/spectrum/response/get_this_spec.rb
@@ -1,56 +1,105 @@
 require_relative '../../spec_helper'
-require_relative '../../doubles/response_get_this_doubles' #empty dependencies
-require './lib/spectrum/response/get_this'
+require 'spectrum/response/get_this'
+
+#collaborators
+require 'spectrum/policy/get_this'
+require 'spectrum/available_online_holding'
+require 'spectrum/holding'
+require 'spectrum/bib_record'
+require 'spectrum/utility/http_client'
+require 'spectrum/utility/solr'
 
 require 'yaml'
 
+class HoldingDouble
+  attr_reader :holdings, :id, :barcode
+  def initialize(holdings, id, barcode)
+    @holdings = holdings
+    @id = id
+    @barcode = barcode
+  end
+end
+class BibRecordDouble
+  attr_reader :solr_response
+  def initialize(solr_response)
+    @solr_response = solr_response
+  end
+end
 
+class GetThisPolicyDouble
+  attr_reader :account, :bib, :item
+  def initialize(account, bib, item)
+    @account = account
+    @bib = bib
+    @item = item
+  end
+  
+ 
+  def resolve
+    self
+  end
+end
+
+class AlephErrorDouble
+end
 
 describe Spectrum::Response::GetThis do
   describe 'renderable' do
     before(:each) do
+      @init = { 
+                source: double("HoldingsSource", holdings: 'http://localhost', url: 'mirlyn_solr_url'),
+                request: double('Spectrum::Request::GetThis', id: '123456789', barcode: '55555', logged_in?: true, username: 'username'),
+                get_this_policy: GetThisPolicyDouble,
+                aleph_borrower: double('Aleph::Borrower', bor_info: [], expired?: false), 
+                aleph_error: AlephErrorDouble,
+                holding: HoldingDouble, bib_record: BibRecordDouble,
+                client: double('Spectrum::Utility::HttpClient', get: nil),
+                solr: double('Spectrum::Utility::Solr'),
+      }
       @holdings_source_dbl = double("HoldingsSource", holdings: 'http://localhost', url: 'mirlyn_solr_url')
       @request_dbl = double('Spectrum::Request::GetThis', id: '123456789', barcode: '55555', logged_in?: true, username: 'username')
     end
-    it 'returns {} if source.holdings is empty' do
-      request_dbl = double('Spectrum::Request::GetThis')
-      empty_holdings_source = double("HoldingsSource", holdings: nil)
 
-      get_this = described_class.new(source: empty_holdings_source, request: request_dbl) 
+
+    it 'returns {} if source.holdings is empty' do
+
+      @init[:source] = double("HoldingsSource", holdings: nil)
+
+      get_this = described_class.new(**@init )
       expect(get_this.renderable).to eq({})
     end
     it 'returns needs_authentication if not logged in' do
-      request_dbl= double('Spectrum::Request::GetThis', logged_in?: false)
-      get_this = described_class.new(source: @holdings_source_dbl, request: request_dbl) 
+      @init[:request] = double('Spectrum::Request::GetThis', logged_in?: false)
+      get_this = described_class.new(**@init) 
       expect(get_this.renderable).to eq( { status: 'Not logged in', options: [] })
     end
 
     it 'returns patron_expired if patron is expired' do
-      expired_aleph_borrower_dbl = double('Aleph::Borrower', bor_info: [], expired?: true)
-      get_this = described_class.new(source: @holdings_source_dbl, request: @request_dbl, aleph_borrower: expired_aleph_borrower_dbl ) 
+      @init[:aleph_borrower]  = double('Aleph::Borrower', bor_info: [], expired?: true)
+      get_this = described_class.new(**@init) 
       expect(get_this.renderable).to eq({ status: 'Patron expired', options: [] })
     end
 
     it 'returns patron_not_found if aleph_error raised'
     
     it 'calls get_this_policy with bib_record' do
-      aleph_borrower_dbl = double('Aleph::Borrower', bor_info: [], expired?: false)
-      client = double('Spectrum::Utility::HttpClient', get: [])
+      @init[:client] = double('Spectrum::Utility::HttpClient', get: [])
       rsolr_client = double( 'RSolr.connect', get: 'mybib' )
-      solr = double('Spectrum::Utility::Solr', connect: rsolr_client, solr_escape: '')
+      @init[:solr] = double('Spectrum::Utility::Solr', connect: rsolr_client, solr_escape: '')
 
-      get_this = described_class.new(source: @holdings_source_dbl, request: @request_dbl, aleph_borrower: aleph_borrower_dbl, client: client, solr: solr) 
+      get_this = described_class.new(**@init) 
             
       expect(get_this.renderable[:options].bib.solr_response).to eq('mybib')
     end
     
     it 'calls get_this_policy with holdings_record' do
       aleph_borrower_dbl = double('Aleph::Borrower', bor_info: [], expired?: false)
-      client = double('Spectrum::Utility::HttpClient', get: 'myholdings')
-      rsolr_client = double( 'RSolr.connect', get: '' )
-      solr = double('Spectrum::Utility::Solr', connect: rsolr_client, solr_escape: '')
 
-      get_this = described_class.new(source: @holdings_source_dbl, request: @request_dbl, aleph_borrower: aleph_borrower_dbl, client: client, solr: solr) 
+      @init[:client] = double('Spectrum::Utility::HttpClient', get: 'myholdings')
+      rsolr_client = double( 'RSolr.connect', get: '' )
+      @init[:solr] = double('Spectrum::Utility::Solr', connect: rsolr_client, solr_escape: '')
+
+      get_this = described_class.new(**@ini) 
             
       expect(get_this.renderable[:options].item.holdings).to eq('myholdings')
       expect(get_this.renderable[:options].item.id).to eq('123456789')
