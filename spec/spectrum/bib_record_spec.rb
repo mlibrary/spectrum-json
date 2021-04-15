36edf5856dcb4f00e6943be4e6ccfa960a0d650d
Monique Rio <mrio@umich.edu>
2021-04-15 11:35:32 -0400

    updated bib record spec to use alma data

--- spec/spectrum/bib_record_spec.rb
+++ spec/spectrum/bib_record_spec.rb
@@ -4,66 +4,66 @@ require_relative '../spec_helper'
 
 describe Spectrum::BibRecord do
   subject do
-    described_class.new(YAML.load_file(File.expand_path('../bib_record.yml', __FILE__)))
+    described_class.new(JSON.parse(File.read('./spec/fixtures/solr_bib_alma.json')))
   end
 
   context '#title' do
     it 'returns a string' do
-      expect(subject.title).to eq('The materials of aircraft construction, for the designer, user and student of aircraft and aircraft engines,')
+      expect(subject.title).to eq('Enhancing faculty careers : strategies for development and renewal /')
     end
   end
 
   context '#issn' do
     it 'returns a string' do
-      expect(subject.issn).to eq('N/A')
+      expect(subject.issn).to eq('')
     end
   end
 
   context '#isbn' do
     it 'returns a string' do
-      expect(subject.isbn).to eq('')
+      expect(subject.isbn).to eq('9781555422103')
     end
   end
 
   context '#bib.accession_number' do
     it 'returns a string' do
-      expect(subject.accession_number).to eq('<accession_number>03935616</accession_number>')
+      expect(subject.accession_number).to eq('<accession_number>20758549</accession_number>')
     end
   end
 
   context '#author' do
     it 'returns a string' do
-      expect(subject.author).to eq('Hill, Frederick Thomas, 1886-')
+      expect(subject.author).to eq('Schuster, Jack H.')
     end
   end
 
   context '#date' do
     it 'returns a string' do
-      expect(subject.date).to eq('1940')
+      expect(subject.date).to eq('1990')
     end
   end
 
   context '#pub' do
     it 'returns a string' do
-      expect(subject.pub).to eq('Sir I. Pitman & sons, ltd.')
+      expect(subject.pub).to eq('Jossey-Bass Publishers')
     end
   end
 
   context '#place' do
     it 'returns a string' do
-      expect(subject.place).to eq('London')
+      expect(subject.place).to eq('San Francisco ')
     end
   end
 
   context '#edition' do
     it 'returns a string' do
-      expect(subject.edition).to eq('4th and rev. ed.')
+      expect(subject.edition).to eq('1st ed.')
     end
   end
 
   context '#callnumber' do
     it 'returns a string' do
-      expect(subject.callnumber).to eq('TL 698 .H64 1940')
+      expect(subject.callnumber).to eq('LB 2331.72 .S371 1990')
     end
   end
 end
