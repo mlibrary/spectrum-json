85fac934755acb8a95b50eebf10b57a094740cd2
Monique Rio <mrio@umich.edu>
2021-06-15 09:28:31 -0400

    commented out LinkedHoldings since they aren't currently implemented

--- lib/spectrum/presenters/holding_presenter.rb
+++ lib/spectrum/presenters/holding_presenter.rb
@@ -123,51 +123,52 @@ module Spectrum::Presenters
       end
     end
   end
-  class LinkedHoldingPresenter < HoldingPresenter
-    def self.for(input)
-      if input.holding.down_links
-        DownLinkedHolding.new(input)
-      else
-        UpLinkedHolding.new(input)
-      end
-    end
+  #Will bring this back at some point, but right now it's not being used
+  #class LinkedHoldingPresenter < HoldingPresenter
+    #def self.for(input)
+      #if input.holding.down_links
+        #DownLinkedHolding.new(input)
+      #else
+        #UpLinkedHolding.new(input)
+      #end
+    #end
 
-    private
-    def headings
-        ['Record link']
-    end
-    def name
-      nil
-    end
+    #private
+    #def headings
+        #['Record link']
+    #end
+    #def name
+      #nil
+    #end
 
-    def process_link(link)
-      [
-        {
-          text: link['link_text'],
-          to: {
-            record: link['key'],
-            datastore: @holding.doc_id,
-          }
-        }
-      ]
-    end
-  end
-  class DownLinkedHolding < LinkedHoldingPresenter
-    private
-    def caption
-      'Bound with'
-    end
-    def rows
-      @holding.down_links.map { |link| process_link(link) }
-    end
-  end
-  class UpLinkedHolding < LinkedHoldingPresenter
-    private
-    def caption
-      'Included in'
-    end
-    def rows
-      @holding.up_links.map { |link| process_link(link) }
-    end
-  end
+    #def process_link(link)
+      #[
+        #{
+          #text: link['link_text'],
+          #to: {
+            #record: link['key'],
+            #datastore: @holding.doc_id,
+          #}
+        #}
+      #]
+    #end
+  #end
+  #class DownLinkedHolding < LinkedHoldingPresenter
+    #private
+    #def caption
+      #'Bound with'
+    #end
+    #def rows
+      #@holding.down_links.map { |link| process_link(link) }
+    #end
+  #end
+  #class UpLinkedHolding < LinkedHoldingPresenter
+    #private
+    #def caption
+      #'Included in'
+    #end
+    #def rows
+      #@holding.up_links.map { |link| process_link(link) }
+    #end
+  #end
 end
