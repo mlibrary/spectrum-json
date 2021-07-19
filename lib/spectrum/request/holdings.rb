5125ba572887b5f7176a4ec3b119db82e7c22a6d
Anthony Thomas <estelendur@gmail.com>
2021-07-19 13:04:46 -0400

    SEARCH-1358: redirect from old aleph_id to new alma id (#25)
    If we get a 9-digit id on a mirlyn single-record query, change the id_field for both the record query and the holdings query to aleph_id.
    
    There's an apparently extraneous `id_field ||= 'id'` line in BibRecord.fetch because if that error ever comes up (someone passes in an id_field that's nil) it will be super annoying to diagnose. As little behavior change as possible here.

--- lib/spectrum/request/holdings.rb
+++ lib/spectrum/request/holdings.rb
@@ -3,11 +3,16 @@
 module Spectrum
   module Request
     class Holdings
-      attr_reader :id, :focus
+      attr_reader :id, :focus, :id_field
 
       def initialize(request)
         @id = request[:id]
-        @focus = request[:id]
+        @focus = request[:focus]
+        if @focus == 'mirlyn' and @id.length == 9
+          @id_field = 'aleph_id'
+        else
+          @id_field = request[:id_field] || 'id'
+        end
       end
 
       def can_sort?
